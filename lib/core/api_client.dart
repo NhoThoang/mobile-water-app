db101c799e26da774421d86a8dcd96cdb2ac93f9
thoang <finncredit@gmail.com>
2026-05-03 14:09:40 +0700

    update

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -5,7 +5,7 @@ import 'logger_service.dart';
 class ApiClient {
   late Dio dio;
   final storage = const FlutterSecureStorage();
-  final String baseUrl = "http://10.0.2.2:8000/api/v1"; // 10.0.2.2 cho Android Emulator
+  final String baseUrl = "https://thoang5.nhothoang.store/api/v1";
 
   ApiClient() {
     dio = Dio(BaseOptions(baseUrl: baseUrl));
